4e12d11330b2921d406ea813cfe543e62a9fe6dc
Nikhil Marathe <nsm.nikhil@gmail.com>
2009-05-14 21:17:53 +0530

    Added support for constraining rectangle, made Sprite constructor more defined

--- src/sprite.js
+++ src/sprite.js
@@ -1,34 +1,51 @@
 var Sprite = Class.create();
 Object.extend(Sprite.prototype, {
-
-    initialize : function(x, y, img) {
-        this.x = x || 0;
-        this.y = y || 0;
-        this.dx = 0;
-        this.dy = 0;
-        this.image = img || null;
+    dx : 0,
+    dy : 0,
+    rect : null,
+    image : null,
+    constraints : null,
+    
+    // imgid should be id of img/canvas
+    initialize : function(x, y, imgid, dx, dy, constraints) {
+        this.dx = dx || 0;
+        this.dy = dy || 0;
+        this.image = document.getElementById(imgid);
+        this.rect = rect(x || 0,
+                         y || 0,
+                         this.image ? this.image.width : 0,
+                         this.image ? this.image.height : 0);
+        
+        this.constraints = constraints || rect(0, 0, C.WORLD_WIDTH, C.WORLD_HEIGHT);
     },
 
     draw : function(canvas) {
-        canvas.drawImage(this.image, this.x, this.y, this.image.width, this.image.height);
+        canvas.drawImage(this.image, this.rect.x, this.rect.y, this.image.width, this.image.height);
     },
     
     animate : function(canvas) {
-        this.x += this.dx;
-        this.y += this.dy;
-        if ( this.x > 560 || this.x < /*110*/ 0 )
-            this.dx = -this.dx;
-        if ( this.y > 420 || this.y < 30 )
-            this.dy = -this.dy
+        this.rect.x += this.dx;
+        this.rect.y += this.dy;
         this.draw(canvas);
-    },
-    
-    rect : function() {
-        return { x : this.x, y : this.y, width : this.image.width, height : this.image.height };
+        
+        if( !this.constraints )
+            return;
+            
+        if ( this.rect.x + this.rect.width > this.constraints.x + this.constraints.width )
+            this.dx = -Math.abs(this.dx);
+            
+        if( this.rect.x < this.constraints.x )
+            this.dx = Math.abs(this.dx);
+            
+        if ( this.rect.y + this.rect.height > this.constraints.y + this.constraints.height )
+            this.dy = -Math.abs(this.dy);
+            
+        if( this.rect.y < this.constraints.y )
+            this.dy = Math.abs(this.dy);
     },
     
     collidepoint : function(x, y) {
-        r = this.rect();
+        r = this.rect;
         if( x < r.x || x > r.x + r.width )
             return false;
         if( y < r.y || y > r.y + r.height )
@@ -37,7 +54,7 @@ Object.extend(Sprite.prototype, {
     },
     
     colliderect : function(rect) {
-        r = this.rect();
+        r = this.rect;
         
         if( r.x + r.width < rect.x ) return false;
         if( r.x > rect.x + rect.width ) return false;
@@ -45,13 +62,24 @@ Object.extend(Sprite.prototype, {
         if( r.y > rect.y + rect.height ) return false;
         
         return true;
+    },
+    
+    toString : function() {
+        return "Sprite";
     }
 });
+Object.inherits(Sprite, Object);
 
 /*** Spaghetti monsters ***/
 var Monster = Class.create();
-Object.extend(Monster.prototype, new Sprite());
+Object.extend(Monster.prototype, Sprite.prototype);
 Object.extend(Monster.prototype, {
-    initialize : function(x, y, img) {
+    initialize : function() {
+        this._super.initialize.apply(this, arguments);
+    },
+    
+    toString : function() {
+        return "Monster";
     }
 });
+Object.inherits(Monster, Sprite);
