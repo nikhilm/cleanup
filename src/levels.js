49e8422317506602474cbfaaaf0f521473fb76c7
Nikhil Marathe <nsm.nikhil@gmail.com>
2009-05-26 17:47:12 +0530

    Added temporary bullet image and empty levels map

---
+++ src/levels.js
@@ -0,0 +1,28 @@
+var Levels = [
+/**
+ * Level description:
+ *  '#' is one touch plate, '*' is two touch
+ *  's' is start position
+ */
+
+// LEVEL 0
+{
+    name : "",
+    map : [ "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               " ]
+}
+
+];
