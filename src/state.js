182d1b6f51260951e9cb4386340f8e7f76b581c9
Nikhil Marathe <nsm.nikhil@gmail.com>
2009-06-02 15:44:20 +0530

    Added MenuState which doesn't do anything right now

--- src/state.js
+++ src/state.js
@@ -10,3 +10,21 @@ Object.extend( State.prototype, {
     draw : function(canvas) {}
 });
 Object.inherits(State, Object);
+
+var MenuState = Class.create();
+Object.extend( MenuState.prototype, State.prototype );
+Object.extend( MenuState.prototype, {
+    name : "Menu",
+    
+    initialize : function() {
+        this._super.apply(this, arguments);
+    },
+    
+    draw : function() {
+    },
+               
+    toString : function() {
+        return this.name;
+    }
+});
+Object.inherits( MenuState, State );
