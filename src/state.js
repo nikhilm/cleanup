d7948257bf301dd38eff307472f3a6ea8cfbf214
Nikhil Marathe <nsm.nikhil@gmail.com>
2009-06-05 12:16:00 +0530

    Implemented MenuState
    MenuState draws the menu items and handles mouse click/hover to select
    , deselect and activate them as required. activate() methods for the menu
    items are to be added

--- src/state.js
+++ src/state.js
@@ -17,12 +17,45 @@ var MenuState = Class.create();
 Object.extend( MenuState.prototype, State.prototype );
 Object.extend( MenuState.prototype, {
     name : "Menu",
+    items : [],
     
     initialize : function() {
-        this._super.apply(this, arguments);
+        //this._super.apply(this, arguments);
+        
+        var newGame = new MenuItem("new", 150, 50);
+        
+        var loadGame = new MenuItem("load", 150, 130);
+        
+        var highscore = new MenuItem("scores", 150, 210);
+        
+        this.items = [ newGame, loadGame, highscore ];
+        
+    },
+               
+    belowMouse : function(evt) {
+        var cvs = document.getElementById('game-canvas');
+        for( var i = 0; i < this.items.length; i++ )
+            if( this.items[i].collidePoint(evt.clientX - cvs.offsetLeft, evt.clientY - cvs.offsetTop) )
+                return this.items[i];
+        // dummy
+        return new MenuItem(-100, -100);
     },
     
-    draw : function() {
+    hover : function(evt) {
+        for( var i = 0; i < this.items.length; i++ )
+            this.items[i].deselect();
+        this.belowMouse(evt).select();
+    },
+               
+    activate : function(evt) {
+        this.belowMouse(evt).activate();
+    },
+    
+    draw : function(canvas) {
+        
+        this.items.each( (function(it) {
+            it.draw(canvas);
+        }).bind(this) );
     },
                
     toString : function() {
